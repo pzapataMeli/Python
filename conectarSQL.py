a027bd031b806eca799a44eb35a844e8dc55d5b9
Unknown <pedro.zapata@mercadolibre.com.ve>
2016-12-03 21:02:51 -0400

    Archivo para conectar a BD
    Creacion del archivo para conectarse a la base de datos

---
+++ conectarSQL.py
@@ -0,0 +1,29 @@
+import MySQLdb
+
+DB_HOST = 'localhost'
+DB_USER = 'root'
+DB_PASS = 'master'
+DB_NAME = 'prestamo'
+
+
+def run_query(query=''):
+    datos = [DB_HOST, DB_USER, DB_PASS, DB_NAME]
+
+    conn = MySQLdb.connect(*datos)  # Conectar a la base de datos
+    cursor = conn.cursor()  # Crear un cursor
+    cursor.execute(query)  # Ejecutar una consulta
+
+    if query.upper().startswith('SELECT'):
+        data = cursor.fetchall()  # Traer los resultados de un select
+    else:
+        conn.commit()  # Hacer efectiva la escritura de datos
+        data = None
+
+    cursor.close()  # Cerrar el cursor
+    conn.close()  # Cerrar la conexion
+
+    return data
+
+query = "SELECT * FROM recursos"
+result = run_query(query)
+print result
